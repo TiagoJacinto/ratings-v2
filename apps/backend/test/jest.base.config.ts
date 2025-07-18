c9f69dee379b3dcbce201bf4e6a2994346f9f899
Tiago Jacinto <tiagojacintodev@gmail.com>
2025-07-18 18:22:55 +0100

    setup turborepo

--- apps/backend/test/jest.base.config.ts
+++ apps/backend/test/jest.base.config.ts
@@ -2,6 +2,4 @@ import { type Config } from 'jest';
 import { pathsToModuleNameMapper } from 'ts-jest';
 import { compilerOptions } from '../tsconfig.json';
 
-export default {
-  
-} satisfies Config;
+export default {} satisfies Config;
