ea0de8231050d2a8a470ea2f876b8c4215022435
Tiago Jacinto <tiagojacintodev@gmail.com>
2025-07-30 12:12:58 +0100

    feat(backend): add swagger support

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+		.setTitle("Ratings API")
+		.setVersion("1.0")
+		.build();
+
+	SwaggerModule.setup("api", app, () =>
+		SwaggerModule.createDocument(app, config),
+	);
+
 	app.useGlobalPipes(new ValidationPipe());
+
 	await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
