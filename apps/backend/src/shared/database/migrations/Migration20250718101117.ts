529e3a22a917362f646412b565aa543b05688a67
Tiago Jacinto <tiagojacintodev@gmail.com>
2025-07-30 12:27:22 +0100

    chore(backend): change MikroORM migrations directory

--- apps/backend/src/shared/database/migrations/Migration20250718101117.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from "@mikro-orm/migrations";
-
-export class Migration20250718101117 extends Migration {
-	override up() {
-		this.addSql(
-			`create table "category" ("id" serial primary key, "name" varchar(100) not null, "description" text null);`,
-		);
-	}
-}
