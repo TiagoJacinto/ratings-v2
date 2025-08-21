91611525f1352f1c49af6c7700cf9fbeec608c0e
Tiago Jacinto <tiagojacintodev@gmail.com>
2025-08-21 13:24:03 +0100

    chore(features): error if no path argument is passed to scripts

--- packages/features/scripts/steps.ts
+++ packages/features/scripts/steps.ts
@@ -1,6 +1,7 @@
 import { generateCodeFromFeature } from "../src/utils";
 
 const path = process.argv[2];
+if (!path) throw new Error("Pass a path argument");
 
 const code = generateCodeFromFeature(path, {
 	type: "steps",
